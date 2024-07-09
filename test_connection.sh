b7ea16eb7f9395d8bc21286c38bbb510a1c0c628
pangz <pangz.lab@gmail.com>
2024-07-10 00:46:44 +0900

    updated config and README
    - renamed connection tester
    - update .dockerignore
    - removed unused comments

--- test_connection.sh
+++ test_connection.sh
@@ -1,8 +1,10 @@
-PORT=7777
+PORT=$1
 echo ""
 echo "++++++++++++++++++++++++++++++++++++++++++"
 echo " Make sure you're using the correct port! "
 echo "++++++++++++++++++++++++++++++++++++++++++"
+echo ""
+echo "[ Running in port ${PORT} ]"
 echo ""
 curl --location "http://127.0.0.1:${PORT}" \
 --data '{"jsonrpc": "1.0", "id":"rpc_server_test", "method": "getblockchaininfo", "params": [] }'
